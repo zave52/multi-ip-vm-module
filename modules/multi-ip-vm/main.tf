9b901efcb7264d470c2a9819cf384473a65b2e01
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-12-05 15:15:34 +0200

    Add Azure Terraform resources for virtual network, subnet, and public IPs

--- modules/multi-ip-vm/main.tf
+++ modules/multi-ip-vm/main.tf
@@ -10,3 +10,68 @@ locals {
     "${local.ip_base}.${local.ip_start_host + i}"
   ]
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.name}-vnet"
+  address_space       = [var.vnet_address_space]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "${var.name}-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.subnet_address_prefix]
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.name}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  dynamic "security_rule" {
+    for_each = var.security_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
+      destination_address_prefix = security_rule.value.destination_address_prefix
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_public_ip" "primary" {
+  name                = "${var.name}-pip-primary"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = var.public_ip_allocation_method
+  sku                 = var.public_ip_sku
+
+  tags = merge(var.tags, {
+    ip_type = "primary"
+  })
+}
+
+resource "azurerm_public_ip" "secondary" {
+  count               = var.secondary_ip_count
+  name                = "${var.name}-pip-secondary-${count.index + 1}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = var.public_ip_allocation_method
+  sku                 = var.public_ip_sku
+
+  tags = merge(var.tags, {
+    ip_type = "secondary"
+    index   = count.index + 1
+  })
+}
--- modules/multi-ip-vm/variables.tf
+++ modules/multi-ip-vm/variables.tf
@@ -1,3 +1,18 @@
+variable "name" {
+  description = "Base name for resources (will be used as prefix)"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
 variable "secondary_ip_count" {
   description = "Number of secondary public IPs"
   type        = number
@@ -6,4 +21,81 @@ variable "secondary_ip_count" {
 variable "private_ip_start" {
   description = "Starting IP address for private IPs"
   type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space for VNet"
+  type        = string
+}
+
+variable "subnet_address_prefix" {
+  description = "Address prefix for subnet"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "security_rules" {
+  description = "List of security rules for NSG"
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+  default = [
+    {
+      name                       = "SSH"
+      priority                   = 1001
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "AllowOutbound"
+      priority                   = 1002
+      direction                  = "Outbound"
+      access                     = "Allow"
+      protocol                   = "*"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+}
+
+variable "public_ip_allocation_method" {
+  description = "Public IP allocation method (Static or Dynamic)"
+  type        = string
+  default     = "Static"
+
+  validation {
+    condition     = contains(["Static", "Dynamic"], var.public_ip_allocation_method)
+    error_message = "Public IP allocation method must be either 'Static' or 'Dynamic'."
+  }
+}
+
+variable "public_ip_sku" {
+  description = "Public IP SKU (Basic or Standard)"
+  type        = string
+  default     = "Standard"
+
+  validation {
+    condition     = contains(["Basic", "Standard"], var.public_ip_sku)
+    error_message = "Public IP SKU must be either 'Basic' or 'Standard'."
+  }
 }
