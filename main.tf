28332f3c1f3bc3a28d698f3436fad7ce61976fc3
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-12-05 19:00:52 +0200

    Update Azure provider version to 3.0 and format multi-IP VM module parameters

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.0"
     }
   }
 }
@@ -20,15 +20,15 @@ resource "azurerm_resource_group" "main" {
 module "multi_ip_vm" {
   source = "./modules/multi-ip-vm"
 
-  name                        = var.name
-  resource_group_name         = azurerm_resource_group.main.name
-  location                    = azurerm_resource_group.main.location
-  secondary_ip_count          = var.secondary_ip_count
-  admin_username              = var.admin_username
-  ssh_public_key              = var.ssh_public_key
-  vm_size                     = var.vm_size
-  vnet_address_space          = var.vnet_address_space
-  subnet_address_prefix       = var.subnet_address_prefix
-  private_ip_start            = var.private_ip_start
-  tags                        = var.tags
+  name                  = var.name
+  resource_group_name   = azurerm_resource_group.main.name
+  location              = azurerm_resource_group.main.location
+  secondary_ip_count    = var.secondary_ip_count
+  admin_username        = var.admin_username
+  ssh_public_key        = var.ssh_public_key
+  vm_size               = var.vm_size
+  vnet_address_space    = var.vnet_address_space
+  subnet_address_prefix = var.subnet_address_prefix
+  private_ip_start      = var.private_ip_start
+  tags                  = var.tags
 }
