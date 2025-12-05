1658a591291e75c85b6240a52a277fe563c8fda7
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-12-05 15:46:49 +0200

    Add Azure resource group and multi-IP VM module configuration in Terraform

--- main.tf
+++ main.tf
@@ -10,3 +10,25 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+module "multi_ip_vm" {
+  source = "./modules/multi-ip-vm"
+
+  name                        = var.name
+  resource_group_name         = azurerm_resource_group.main.name
+  location                    = azurerm_resource_group.main.location
+  secondary_ip_count          = var.secondary_ip_count
+  admin_username              = var.admin_username
+  ssh_public_key              = var.ssh_public_key
+  vm_size                     = var.vm_size
+  vnet_address_space          = var.vnet_address_space
+  subnet_address_prefix       = var.subnet_address_prefix
+  private_ip_start            = var.private_ip_start
+  tags                        = var.tags
+}
